f7441553cda2dae1663a4f9a2bf5dfff4e61f4c9
Joy Cheng <joycheng@ucsb.edu>
2026-05-19 18:12:49 -0700

    joy - WIP add in courses form

---
+++ frontend/src/main/components/Courses/CoursesForm.jsx
@@ -0,0 +1,90 @@
+import { Button, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router";
+
+function CoursesForm({
+  initialContents,
+  submitAction,
+  buttonLabel = "Create",
+}) {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    defaultValues: initialContents || {},
+  });
+
+  const navigate = useNavigate();
+  const testIdPrefix = "CoursesForm";
+
+  return (
+    <Form onSubmit={handleSubmit(submitAction)}>
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="code">Course Code</Form.Label>
+        <Form.Control
+          data-testid={testIdPrefix + "-"}
+          id="code"
+          type="text"
+          isInvalid={Boolean(errors.code)}
+          {...register("code", {
+            required: "Course code is required.",
+          })}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.code?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="name">Course Name</Form.Label>
+        <Form.Control
+          data-testid={testIdPrefix + "-name"}
+          id="name"
+          type="text"
+          isInvalid={Boolean(errors.orgTranslation)}
+          {...register("name", {
+            required: "Course name is required.",
+            maxLength: {
+              value: 255,
+              message: "Max length 255 characters",
+            },
+          })}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.name?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="term">Course Term</Form.Label>
+        <Form.Control
+          data-testid={testIdPrefix + "-term"}
+          id="term"
+          type="text"
+          isInvalid={Boolean(errors.orgTranslationShort)}
+          {...register("term", {
+            required: "Course term is required.",
+          })}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.term?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      <Button type="submit" data-testid={testIdPrefix + "-submit"}>
+        {buttonLabel}
+      </Button>
+
+      <Button
+        variant="Secondary"
+        onClick={() => navigate(-1)}
+        data-testid={testIdPrefix + "-cancel"}
+      >
+        Cancel
+      </Button>
+    </Form>
+  );
+}
+
+export default CoursesForm;
