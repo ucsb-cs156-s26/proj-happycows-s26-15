ad3dedff8c235e790311f8e33ce0d7d11869765f
Leo Liu <yihengliu@ucsb.edu>
2026-05-23 20:02:34 -0700

    Remove mistakenly added enum values from CommonsFeatures

--- src/test/java/edu/ucsb/cs156/happiercows/enums/CommonsFeaturesTests.java
+++ src/test/java/edu/ucsb/cs156/happiercows/enums/CommonsFeaturesTests.java
@@ -17,36 +17,7 @@ public class CommonsFeaturesTests {
     @Test
     public void features_values_has_single_entry_leaderboard() {
         CommonsFeatures[] values = CommonsFeatures.values();
-        assertEquals(3, values.length);
+        assertEquals(1, values.length);
         assertEquals(CommonsFeatures.FARMERS_CAN_SEE_LEADERBOARD, values[0]);
     }
-
-    @Test
-    public void features_contains_expected_value_herd_size_histogram() {
-        CommonsFeatures feature = CommonsFeatures.valueOf("FARMERS_CAN_SEE_HERD_SIZE_HISTOGRAM");
-        assertNotNull(feature);
-        assertEquals(CommonsFeatures.FARMERS_CAN_SEE_HERD_SIZE_HISTOGRAM, feature);
-    }
-
-    @Test
-    public void features_values_has_single_entry_herd_size_histogram() {
-        CommonsFeatures[] values = CommonsFeatures.values();
-        assertEquals(3, values.length);
-        assertEquals(CommonsFeatures.FARMERS_CAN_SEE_HERD_SIZE_HISTOGRAM, values[1]);
-    }
-
-    @Test
-    public void features_contains_expected_value_taxes() {
-        CommonsFeatures feature = CommonsFeatures.valueOf("TAXES_ON_HERD_SIZE_ARE_ENABLED");
-        assertNotNull(feature);
-        assertEquals(CommonsFeatures.TAXES_ON_HERD_SIZE_ARE_ENABLED, feature);
-    }
-
-    @Test
-    public void features_values_has_single_entry_taxes() {
-        CommonsFeatures[] values = CommonsFeatures.values();
-        assertEquals(3, values.length);
-        assertEquals(CommonsFeatures.TAXES_ON_HERD_SIZE_ARE_ENABLED, values[2]);
-    }
 }
-
